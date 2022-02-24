fdfbd2b0bd7be43f85c2a045747919a06128736d
proofpoint community <100342958+pfptcommunity@users.noreply.github.com>
2022-02-24 19:56:44 +0100

    Update tap_api_private.py

--- tap_api_private.py
+++ tap_api_private.py
@@ -1,1 +1,25 @@
-todo
+import http.client
+import urllib
+import requests
+import basicauth
+import base64
+import json
+
+req ={
+    'method'          : 'GET', 
+    'principal'       : '•••••••••', 
+    'secret'          : '•••••••••', 
+    'uri'             : 'https://tap-api-v2.proofpoint.com', 
+    'command'         : '/v2/people/vap', 
+    'parameters'      : '?window=90'
+   }
+
+userpass   = req['principal'] + ':' + req['secret']
+encoded_u  = base64.b64encode(userpass.encode()).decode()
+headers    = {'Authorization' : 'Basic %s' % encoded_u}
+response   = requests.get(req['uri'] + req['command'] + req['parameters'], headers=headers)
+res        = response.json()
+print(res)
+
+
+
