8dd2fac294af59db92d16e112e5cd9fb127916c4
proofpoint community <100342958+pfptcommunity@users.noreply.github.com>
2022-03-01 14:32:58 +0100

    Update psat_api_private.js

--- psat_api_private.js
+++ psat_api_private.js
@@ -1,1 +1,17 @@
-todo
+const axios = require('axios'); 
+
+const req ={
+    method          : 'GET', 
+    key             : '•••••••••', 
+    uri             : 'https://results.eu.securityeducation.com:443', 
+    command         : '/api/reporting/v0.1.0/cyberstrength', 
+    parameters      : ''
+  }
+
+const params        = {headers: {'x-apikey-token': req.key }}
+
+main(); // calling async function to make await possible
+async function main () {
+  let eti = await axios.get(req.uri+req.command+req.parameters, params       );
+  console.log(eti); 
+} 
