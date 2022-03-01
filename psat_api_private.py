4879f22ec88bf54b5cddbbd8ff69ee36d1dd8489
proofpoint community <100342958+pfptcommunity@users.noreply.github.com>
2022-03-01 14:37:54 +0100

    Update psat_api_private.py

--- psat_api_private.py
+++ psat_api_private.py
@@ -1,1 +1,20 @@
-todo
+import http.client
+import urllib
+import requests
+import json
+
+req ={
+    'method'          : 'GET', 
+    'key'             : '•••••••••', 
+    'uri'             : 'https://results.eu.securityeducation.com:443', 
+    'command'         : '/api/reporting/v0.1.0/cyberstrength', 
+    'parameters'      : ''
+   }
+
+
+headers    = {'x-apikey-token' : req['key']}
+response   = requests.get(req['uri'] + req['command'] + req['parameters'], headers=headers)
+res        = response.json()
+print(res)
+
+
