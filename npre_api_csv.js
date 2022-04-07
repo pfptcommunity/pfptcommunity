f42c41e9eb54cd2e826ac7eb466abc4ecadadfe7
proofpoint community <100342958+pfptcommunity@users.noreply.github.com>
2022-04-07 08:16:49 +0200

    Update npre_api_csv.js

--- npre_api_csv.js
+++ npre_api_csv.js
@@ -1,3 +1,7 @@
-* todo
+// * todo
+// method                = POST
+// request1              = Get the Bearer Token
+// request2              = Get the Uri to CSV file using the Bearer Token
+// request3              = Get CSV file
 
 
