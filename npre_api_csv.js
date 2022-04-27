2ea4093c14a92475c5f4a36257e436e04b27d192
Proofpoint GitHub Community <100342958+pfptcommunity@users.noreply.github.com>
2022-04-27 10:27:08 +0200

    Update npre_api_csv.js

--- npre_api_csv.js
+++ npre_api_csv.js
@@ -1,3 +1,85 @@
 
 // Google Script is based on javascript, see:
 // https://github.com/pfptcommunity/pfptcommunity/blob/main/npre_api_csv.gs
+
+
+
+  /** 
+   * GetAccessToken (param1, params2, param3)  [Get the Access Token from endpoint]
+   * @param1  {[string]}  principal            [api key] 
+   * @param2  {[string]}  secret               [api secret]           
+   * @param3  {[string]}  tokenuri             [path, this should be "https://auth.proofpoint.com/v1/token"]           
+   * @return  {[string]}  access_token         [access token]
+  */
+ async function GetAccessToken(key,secret,uri){
+  const params    = {
+        method             : 'POST',
+        muteHttpExceptions : true,
+        headers : {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        payload : "grant_type=client_credentials&client_id="+key+"&client_secret="+secret
+  };
+  const response=await axios.get(uri, params);
+  console.log(response);
+  return JSON.parse(response);
+} // ** End GetAccessToken
+
+
+/** 
+ * GetFileName (token,apiuri,time_series)    [Get the Filename to the CSV file]
+ * @param1  {[string]}  token                [Bearer Access Token] 
+ * @param2  {[string]}  apiuri               [path, this should be "https://api.peoplecentric.proofpoint.com/graphql"]           
+ * @param3  {[string]}  time_series          [time series / date for the query. Example is 20210209"]           
+ * @return  {[string]}  access_token         [file reference]
+*/
+async function GetFileName(token,uri,ts){
+  const my_payload  = {
+        'operationName': 'getRiskPosture',
+        'variables'    : {'time_series': ts.toString()},
+        'query'        : 'query getRiskPosture($time_series: DateTime){getRiskPosture(time_series: $time_series){file}}'
+      },    
+        params    = {
+          method             : 'POST',
+          muteHttpExceptions : true,
+          headers : {
+            'Content-Type' : 'application/json',
+            'Authorization': 'Bearer ' + token
+          },
+          payload : JSON.stringify(my_payload)
+        };
+  return JSON.parse(await axios.get(uri,params));
+} // ** End GetFileName
+
+
+
+
+const axios = require('axios'); 
+
+// ** MAIN   
+const now    = new Date(),
+      req    = {
+        principal   :  '••••••••',
+        secret      :  '••••••••',
+        timeseries  :  now.getFullYear()+'0'+(now.getMonth()+1).toString()+now.getDate(),  
+        tokenuri    :  'https://auth.proofpoint.com/v1/token',
+        apiuri      :  'https://api.peoplecentric.proofpoint.com/graphql'
+      };
+
+// ** Get the Bearer Token
+req.tok = GetAccessToken(req.principal, req.secret,req.tokenuri);
+console.log("***********GetAccessToken");
+console.log(req.tok.access_token);
+
+
+// ** Get the filename of the csv file
+const response = GetFileName(req.tok.access_token, req.apiuri,req.timeseries);
+console.log("***********GetFileName");
+console.log(response);
+
+// ** Read the csv file
+//  const npredata = //UrlFetchApp.fetch(response.data.getRiskPosture.file).getContentText();
+//  Logger.log(npredata);
+
+
+
